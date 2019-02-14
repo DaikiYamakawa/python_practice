ef9eeecfc119ce13167211af2c16e42cdd0fcf86
DaikiYamakawa <is0333vs@gmail.com>
2019-02-14 17:09:06 +0900

    20190214

--- object_test.py
+++ object_test.py
@@ -6,6 +6,14 @@ print(d.sqrt())
 class Myclass: 
     pass
 
+class Myclass2:
+    def __init__(self): # 初期化メソッドを定義
+        self.value = 0  # インスタンスにアトリビュートを追加
+        print('This is __init() method !')
+
 i = Myclass() # Myclassというクラス名からインスタンスを作成し変数iに代入
 i.value = 5
 print(i.value)
+
+i3 = Myclass2()
+print(i3.value)
