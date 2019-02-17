cba245af762bfa19f54c058bd1e79952b439fa94
DaikiYamakawa <39241071+DaikiYamakawa@users.noreply.github.com>
2019-02-18 01:00:53 +0900

    share lab pc

--- object_test.py
+++ object_test.py
@@ -11,9 +11,25 @@ class Myclass2:
         self.value = 0  # インスタンスにアトリビュートを追加
         print('This is __init() method !')
 
+class Prism:
+    def __init__(self, width, height, depth):
+        self.width = width
+        self.height = height
+        self.depth = depth
+    
+    # 初期化メソッドにてインスタンスのアトリビュートにデータを保存しているので引数はselfのみ
+    def content(self): 
+        return self.width*self.height*self.depth
+
 i = Myclass() # Myclassというクラス名からインスタンスを作成し変数iに代入
 i.value = 5
 print(i.value)
 
 i3 = Myclass2()
 print(i3.value)
+
+p1 = Prism(10, 10, 10)
+p1.height = 20
+ans = p1.content()
+print(ans)
+
